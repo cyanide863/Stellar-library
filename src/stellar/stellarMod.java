3c879cc6d0119822c9b45993b5a0bd7eeaa6501a
C4YNIDE <156609453+cyanide863@users.noreply.github.com>
2024-12-08 16:50:37 +0400

    Update stellarMod.java

--- src/stellar/stellarMod.java
+++ src/stellar/stellarMod.java
@@ -1,11 +1,38 @@
-package template;
+package stellar;
 
+import arc.Events;
+import stellar.graphics.ExoShaders;
+import mindustry.game.EventType;
 import mindustry.mod.*;
-import template.gen.*;
+import stellar.util.util.Utils;
+import stellar.content.StellStatusEffects;
+import stellar.content.StellWeathers;
+import stellar.content.*;
+import mindustry.mod.Mod;
+import mindustry.mod.Mods;
+import stellar.gen.*;
 
-public class ModTemplate extends Mod{
+import static arc.Core.app;
+
+public class
+ExogenesisMod extends Mod{
+    public static Mods.LoadedMod modInfo;
+    public ExogenesisMod(){
+        super();
+
+        Events.on(EventType.FileTreeInitEvent.class, e ->
+                app.post(StellShaders::load)
+        );
+
+        Events.on(EventType.DisposeEvent.class, e ->
+                StellShaders.dispose()
+        );
+    }
     @Override
     public void loadContent(){
         EntityRegistry.register();
+        Utils.init();
+        StellStatusEffects.load();
+        StellWeathers.load();
     }
 }
